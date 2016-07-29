fd405e0e4f146a51006e8947a0b00a117afca886
GingerBear <bbbear444@gmail.com>
2016-07-28 21:13:32 -0400

    add linux run bash

---
+++ run-linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nodejs index.js
+cp -R build/* ./gingerbear.github.io/movies
+cd ./gingerbear.github.io/
+git add .
+export NOW=`date +"%Y-%m-%dT%H:%M:%SZ"`
+git commit -am "update movies data $NOW"
+git push origin master
